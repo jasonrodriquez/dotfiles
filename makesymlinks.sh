48a604e4e71eeacd45ae19d2e5c6dcad24248df4
h0bbit <h0bbit@Prometheus.rod>
2018-12-12 15:28:32 -0700

    adjusted path in makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/git/dotfiles                    # dotfiles directory
+olddir=~/git/dotfiles_old             # old dotfiles backup directory
 files="bash_profile vimrc"    # list of files/folders to symlink in homedir
 
 ##########
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/.$file ~/dotfiles_old/
+	mv ~/.$file ~/git/dotfiles_old/
 	echo "Creating symlink to $file in home directory."
 	ln -s $dir/$file ~/.$file
 done
